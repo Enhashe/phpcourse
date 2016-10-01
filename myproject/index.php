99f82a55954a6c89bcd1645833510d89dd5387af
Enhashe <pendalf.gray@gmail.com>
2016-10-01 21:00:03 +0700

    Some practice

--- myproject/index.php
+++ myproject/index.php
@@ -1,2 +1,18 @@
 <?php
+//some comments here
 echo 'Hello World!!';
+/*another one
+in two lines*/
+$text = '</br>some text here';
+echo "$text</br>";
+
+/*$list = array(1,2,3,4,5,6,7,8,9,0);
+print_r($list);
+echo "</br>";
+echo implode(',', $list);
+*/
+
+$a = 1;
+$b = 2;
+$c = $a % $b;
+var_export($c);
