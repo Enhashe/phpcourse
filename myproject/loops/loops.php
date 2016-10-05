900399869fde82e1a8915c90ee017040b446e426
Enhashe <pendalf.gray@gmail.com>
2016-10-05 16:41:06 +0700

    Task_2 bugfix(increment expression). Task 3,4,5,6 completed.

--- myproject/loops/loops.php
+++ myproject/loops/loops.php
@@ -1,5 +1,4 @@
 <?php
-session_write_close();
 print("<br>Task_1<br><br>");
 
 for ($i=1; $i < 11; $i++) {
@@ -8,50 +7,48 @@ for ($i=1; $i < 11; $i++) {
 
 print('<br>Task_2<br><br>');
 
-/*$sum = 0;
-for ($i=1; $i <= 112; $i + 3) {
+$sum = 0;
+for ($i=1; $i <=112; $i += 3) { 
 	$sum = $sum + $i;
 }
 print ("Counter is: $i <br> Sum is: $sum<br>");
-*/
+
 print('<br>Task_3<br><br>');
 
-/*for ($i=1; $i <= 10000; $i++) {
+for ($i=1; $i <= 10000; $i++) {
 	if ($i % 5 != 0) {
 		for ($j = 0; $j < strlen($i); $j++) {
 			if (substr($i, $j, 1) == 3) {
-				echo "$i<br>";
+				echo "$i ";
 			}
 		}
 	}
-}*/
+}
 
 print('<br>Task_4<br><br>');
 
+$number_quantity = 3;
 $list = array();
-$list_size = count($list);
-/*for ($i=0; $i < 3; $i++) { 
-	$list[$i] = rand(0,100);
-}*/
-/*for ($i= ; $i < 3; $i++) { 
+for ($i=0; $i < $number_quantity; $i++) { 
 	$number = rand(0,100);
-	if ($number == $list[$i]) {
-		$list[$i] = rand(0,100);
+	for ($j=0; $j < count($list); $j++) { 
+		while ($number == $list[$j]) {
+			$number = rand(0,100);
+		}
 	}
-}*/
-$number = rand(0,100);
-for ($i=0; $i < 3; $i++) { 
 	$list[$i] = $number;
-	while ($list[$i] == ) {
-	# code...
-	}
 }
-
 var_dump($list);
 
 print('<br>Task_5<br><br>');
 
-
+$first = 35;
+$last = 87;
+for ($i=$first; $i <= $last ; $i++) { 
+	if ($i % 7 == 1 && 2 && 5) {
+		echo "$i<br>";
+	}
+}
 
 print('<br>Task_6<br><br>');
 ?>
